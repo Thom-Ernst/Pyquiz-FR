b67e51f5adb585e7c432f604744c5601b831228a
thomE_immalle <thom.ernst@immalle.be>
2016-04-15 14:18:50 +0200

    Nyes

---
+++ PyQuiz/resources/ConsoleColorsClass.py
@@ -0,0 +1,9 @@
+class Markup:
+    purple = '\033[95m'
+    blue = '\033[94m'
+    green = '\033[92m'
+    yellow = '\033[93m'
+    red = '\033[91m'
+    end = '\033[0m' # Code to stop the formats and revert to original, still needs propper testing on behaviour...
+    bold = '\033[1m'
+    ul = '\033[4m'
---
+++ PyQuiz/resources/Track.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Track:
+    def __init__(self, name, description, song, anythingelse):
+        dolorem_ipsum = "Text"
+
+    def playsong(self, ):
+        pygame.dostuff
