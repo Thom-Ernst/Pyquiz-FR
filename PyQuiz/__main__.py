ffcf29cc82b32c3be5067f05d3a9db1c21a0a87f
thomE_immalle <thom.ernst@immalle.be>
2016-04-21 21:37:09 +0200

    Did some stuff, dunno what TODO: Fix response if answer was incorrect!

--- PyQuiz/__main__.py
+++ PyQuiz/__main__.py
@@ -4,14 +4,21 @@ from Track import Track_class
 from ConsoleColorsClass import Markup
 pygame.init()
 
-vivaldi_spring_allegro = Track_class("Lente - Vivaldi", 'music/vivaldi_spring_allegro.ogg')
+firstsong = vivaldi_spring_allegro = Track_class("Lente - Vivaldi", 'music/vivaldi_spring_allegro.ogg')
+secondsong = ode_to_joy = Track_class("Ode Aan de Vreugde", 'music/ode_to_joy.ogg')
+thirdsong = toccata_and_fugue = Track_class("Toccata en Fugue", 'music/toccata_and_fugue.ogg')
+objet1 = 1
+objet2 = 0
+objet3 = 0
+
+keuze1 = firstsong.name
+keuze2 = secondsong.name
+keuze3 = thirdsong.name
 
 score = 0
 
-song = "Vivaldi Spring Allegro"
 
-
-def option1(option, points):
+def option1(option, points, song):
     if option == 1:
         print "Dat is correct!"
         pygame.mixer.music.load('sounds/Nyes.ogg')
@@ -21,10 +28,10 @@ def option1(option, points):
         print "Dat is niet juist!"
         pygame.mixer.music.load('sounds/No.ogg')
         pygame.mixer.music.play()
-        print "Het juiste antwoord is {0}".format(song.name)
+        print "Het juiste antwoord was {0}".format(song.name)
 
 
-def option2(option, points):
+def option2(option, points, song):
     if option == 1:
         print "Dat is correct!"
         pygame.mixer.music.load('sounds/Nyes.ogg')
@@ -34,10 +41,10 @@ def option2(option, points):
         print "Dat is niet juist!"
         pygame.mixer.music.load('sounds/No.ogg')
         pygame.mixer.music.play()
-        print "Het juiste antwoord is {0}".format(song.name)
+        print "Het juiste antwoord was {0}".format(song.name)
 
 
-def option3(option, points):
+def option3(option, points, song):
     if option == 1:
         print "Dat is correct!"
         pygame.mixer.music.load('sounds/Nyes.ogg')
@@ -47,19 +54,53 @@ def option3(option, points):
         print "Dat is niet juist!"
         pygame.mixer.music.load('sounds/No.ogg')
         pygame.mixer.music.play()
-        print "Het juiste antwoord is {0}".format(song.name)
+        print "Het juiste antwoord was {0}".format(song.name)
+
+
+# menu = {
+#     '1': option1(objet1, score, keuze1),
+#     '2': option2(objet2, score, keuze2),
+#     '3': option3(objet3, score, keuze3)
+# }
+
+
+def opt1():
+    pygame.mixer.music.stop()
+    option1(objet1, score, keuze1)
+
+
+def opt2():
+    pygame.mixer.music.stop()
+    option2(objet2, score, keuze2)
+
+
+def opt3():
+    pygame.mixer.music.stop()
+    option3(objet3, score, keuze3)
 
 menu = {
-    '1': option1,
-    '2': option2,
-    '3': option3
+    '1': opt1,
+    '2': opt2,
+    '3': opt3
 }
 
-pygame.mixer.music.load('music/vivaldi_spring_allegro.ogg')
+designated_song = firstsong.song
+if objet1 == 1:
+    designated_song = firstsong.song
+elif objet2 == 1:
+    designated_song = secondsong.song
+elif objet3 == 1:
+    designated_song = thirdsong.song
+else:
+    print "Error: no song was assigned!"
+
+pygame.mixer.music.load(designated_song)
 pygame.mixer.music.play()
 print "De keuzes zijn:"
 print ""
 print "1. {0}".format(keuze1)
 print "2. {0}".format(keuze2)
 print "3. {0}".format(keuze3)
-raw_input("Je antwoord hier: ")
+answer = raw_input("Je antwoord hier: ")
+menu[answer]()
+print score
--- PyQuiz/music/vivaldi_spring_allegro.py
+++
@@ -1,2 +0,0 @@
-from PyQuiz.Track import Track
-vivaldi_spring_allegro = Track("Lente - Vivaldi", 'music/vivaldi_spring_allegro.ogg')
