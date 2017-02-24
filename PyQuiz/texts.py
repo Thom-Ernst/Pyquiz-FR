43fd09ee6df3ea8363a19fc4030348de370a29ab
ThomE-immalle <thom.ernst@immalle.be>
2017-02-24 23:15:07 +0100

    fixed some small things

--- PyQuiz/texts.py
+++ PyQuiz/texts.py
@@ -1,7 +1,9 @@
 # file with all the strings in for future translation to french, this will make things easier.
 
+textBlank = "\n" * 100
+
 textIntro1 = "Welkom op onze muziekquiz!"
-textIntro2 = "Geef je naam op:"
+textIntro2 = "Geef je naam op: "
 textIntro3 = "Laten we beginnen, {0}!"
 
 textRonde1 = ""
