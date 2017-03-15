9fa3182ceb98c9866c5f30be4a3e39aadae402f9
ThomE-immalle <thom.ernst@immalle.be>
2017-03-15 23:58:15 +0100

    added code for startround()

--- PyQuiz/__main__.py
+++ PyQuiz/__main__.py
@@ -14,6 +14,7 @@ intRonde = 0
 # functions go here
 dj = DJ()
 
+
 def intro():
     global boolGameloop
     print textBlank + Layout.bold + textIntro1 + "\n"
@@ -31,24 +32,49 @@ def intro():
 
 
 def einde():
+    print textBlank
     print Layout.bold + textEinde1.format(strNaam, intScore)
     print textEinde2 + Layout.end
     print textEinde3
 
 
 def startround():
-    raw_input()
+    print textBlank
+    print Layout.bold + textRonde1.format(intRonde)
+    dj.chooseListing()
+    dj.chooseTrack()
+    dj.trackChosen.playsong()
+
+    def internalround():
+        global intScore
+        print textRonde2
+        for i in xrange(4):
+            print dj.arrayChosen[i].name
+        answer = raw_input(textRonde3 + Layout.end)
+        if answer == dj.trackChosen.name:
+            dj.soundCorrect.playsong()
+            print Layout.bold + Layout.green + textRonde5 + Layout.end
+            print Layout.bold + textRonde6 + Layout.end
+            intScore += 1
+            raw_input(textEnter)
+        elif answer in dj.arrayChosen.name:
+            dj.soundIncorrect.playsong()
+            print Layout.bold + Layout.red + textRonde4.format(dj.trackChosen.name) + Layout.end
+            raw_input(textEnter)
+        else:
+            print Layout.bold + Layout.red + textError2 + Layout.end
+            raw_input(textEnter)
+            internalround()
+    internalround()
+
 
 intro()
 dj.randomizeArray()
 
 while boolGameloop:
     # all the functions executed in their respective order
-    dj.chooseListing()
-    dj.chooseTrack()
     startround()
     intRonde += 1
-
     if intRonde == 10:
         boolGameloop = False
 
