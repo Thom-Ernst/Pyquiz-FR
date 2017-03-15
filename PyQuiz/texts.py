c5386a5917e319a8896d9bb929c2cf391c9888b6
ThomE-immalle <thom.ernst@immalle.be>
2017-03-15 23:57:31 +0100

    added some strings

--- PyQuiz/texts.py
+++ PyQuiz/texts.py
@@ -1,17 +1,22 @@
 # file with all the strings in for future translation to french, this will make things easier.
 
 textBlank = "\n" * 100
+textEnter = "Druk op enter om door te gaan."
 
 textIntro1 = "Welkom op onze muziekquiz!"
 textIntro2 = "Geef je naam op: "
 textIntro3 = "Laten we beginnen, {0}!"
 
-textRonde1 = ""
-textRonde2 = ""
+textRonde1 = "Ronde {0}. \n Start! \n"
+textRonde2 = "Je keuzes: \n"
+textRonde3 = "Je antwoord hier: "
+textRonde4 = "Fout! \n Het juiste antwoord was {0}!"
+textRonde5 = "Juist!"
+textRonde6 = "Je hebt een punt gescoord!"
 
 textEinde1 = ""
 textEinde2 = ""
 textEinde3 = ""
 
 textError1 = "FOUT: geef een naam op!"
-
+textError2 = "FOUT: geef een getal tussen 1 en 4 op!"
